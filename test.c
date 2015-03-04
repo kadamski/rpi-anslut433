06ddaf0de8f075b2a6b2f9f54502a82a27cd6a7b
Krzysztof Adamski <k@japko.eu>
2015-03-04 22:47:10 +0000

    test: add argument parsing.

--- test.c
+++ test.c
@@ -1,20 +1,82 @@
 #include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 #include "anslut.h"
 
-int main (void)
-{
-    int i;
-    struct device dev;
-    //wiringPiSetupSys();
-    wiringPiSetupGpio();
-    pinMode(22, OUTPUT);
+#define PIN 22
 
-    device_init(&dev, 22, "10101010101010101010101010", T_ANSLUT);
+void setup(int sys)
+{
+
+    if (sys) {
+        wiringPiSetupSys();
+    } else {
+        wiringPiSetupGpio();
+    }
+    pinMode(PIN, OUTPUT);
+}
+
+void usage(void)
+{
+    fprintf(stderr, "Usage: test [-n] [-s] [-p pid] [-u unit] addr\n");
+    fprintf(stderr, "-n\t- NEXA instead of ANSLUT device\n");
+    fprintf(stderr, "-s\t- user sysfs GPIO instead of /dev/mem\n");
+    fprintf(stderr, "-u\t- unit number, 3=group (default)\n");
+    fprintf(stderr, "-p\t- data pin number (default=22)\n");
+    fprintf(stderr, "addr\t- RC addr, for example 10101010101010101010101010\n");
+}
+
+int main(int argc, char *argv[])
+{
+    int opt;
+    int unit = U_G, type = T_ANSLUT, pin = PIN, use_sys = 0;
+    struct device dev;
+
+    while ((opt = getopt(argc, argv, "nsu:p:")) != -1) {
+        switch (opt) {
+        case 'u':
+            unit = atoi(optarg);
+            break;
+        case 'p':
+            pin = atoi(optarg);
+            break;
+        case 's':
+            use_sys = 1;
+            break;
+        case 'n':
+            type = T_NEXA;
+            break;
+        default:
+            usage();
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (optind >= argc) {
+        fprintf(stderr, "No address specified!\n");
+        usage();
+        exit(EXIT_FAILURE);
+    }
+
+    if (strlen(argv[optind]) != 26) {
+        fprintf(stderr, "Address should be 26 bits long!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (unit < 0 || unit > 3) {
+        fprintf(stderr, "Unit should be in range 0-3\n");
+        exit(EXIT_FAILURE);
+    }
+
+    setup(use_sys);
+    device_init(&dev, pin, argv[optind], type);
 
     for(;;) {
-        device_off(&dev, U_1);
+        device_off(&dev, unit);
         delay(1000);
-        device_on(&dev, U_1);
+        device_on(&dev, unit);
         delay(1000);
     }
 
